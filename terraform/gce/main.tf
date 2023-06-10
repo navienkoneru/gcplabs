9a6dd189cfc3d42a671629320515fab9fe9b1149
navienkoneru <131322981+navienkoneru@users.noreply.github.com>
2023-06-09 21:47:31 -0400

    Create main.tf

---
+++ terraform/gce/main.tf
@@ -0,0 +1,36 @@
+provider "google" {
+  project     = var.project
+  region      = var.region
+} 
+
+
+resource "google_compute_instance" "vminstance" {
+  name         = var.name
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags = ["ontario"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-2004-focal-v20230605"
+    }
+  }
+
+  provisioner "local-exec" {
+    command = "gcloud compute instances create hello --zone us-central1-a"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
+
+/*
+resource "google_compute_network" "my-network" {
+  name = "terraform-network"
+}
+*/
