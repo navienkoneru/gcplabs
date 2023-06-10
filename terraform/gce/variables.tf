f881b5d9b54f599fcd1ad65a0a164d91dd05bf50
navienkoneru <131322981+navienkoneru@users.noreply.github.com>
2023-06-09 21:48:05 -0400

    Create variables.tf

---
+++ terraform/gce/variables.tf
@@ -0,0 +1,24 @@
+variable "project" {
+    type = string
+    default = "prjct-navi"
+}
+
+variable "region" {
+    type = string
+    default = "us-central1"
+}
+
+variable "name" {
+    type = string
+    default = "terraform"
+}
+
+variable "machine_type" {
+    type = string
+    default ="e2-medium"
+}
+
+variable "zone" {
+    type = string
+    default = "us-central1-a"
+}
